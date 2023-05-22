317b7ede2653b7e6a5a026a2f9589fbd816ba805
etudiant <Benjamin.valleix@gmail.com>
2023-05-22 11:40:31 +0200

    :anger: Add VM

---
+++ ViewModel/ChampionMgrVM.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Model;
+
+namespace ViewModel
+{
+	public class ChampionMgrVM : INotifyPropertyChanged
+	{
+		public IReadOnlyCollection<ChampionVM> Champions { get; private set; }
+		private ObservableCollection<ChampionVM> champions = new ObservableCollection<ChampionVM>();
+
+		private IDataManager DataManager { get; set; }
+
+		public ChampionMgrVM(IDataManager dataManager)
+		{
+			DataManager = dataManager;
+			Champions = new ReadOnlyCollection<ChampionVM>(champions);
+			PropertyChanged += ChampionMgrVM_PropertyChanged;
+		}
+
+        private async void ChampionMgrVM_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if(e.PropertyName == nameof(Index))
+			{
+				await LoadChampions();
+			}
+        }
+
+        public int Index
+		{
+			get => index;
+			set
+			{
+				if (index == value) return;
+				index = value;
+				OnPropertyChanged();
+			}
+		}
+		private int index;
+
+		public int Count
+		{
+			get;
+			set;
+		} = 5;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private async Task LoadChampions()
+        {
+			champions.Clear();
+			var modelChampions = await DataManager.ChampionsMgr.GetItems(Index, Count);
+            foreach (var n in modelChampions)
+            {
+				champions.Add(new ChampionVM(n));
+
+			}
+        }
+
+    }
+}
+
---
+++ ViewModel/ChampionVM.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Model;
+
+namespace ViewModel;
+
+public class ChampionVM : INotifyPropertyChanged
+{
+    public ChampionVM(Champion champion) => Model = champion;
+
+    public ChampionVM() => Model = new Champion("Heros", ChampionClass.Assassin);
+
+    private Champion model;
+    public Champion Model
+    {
+        get => model;
+        set
+        {
+            if (model.Equals(value)) return;
+            model = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Name
+    {
+        get => Model?.Name;
+    }
+
+    public string Icon
+    {
+        get => Model?.Icon;
+        set
+        {
+            if (model == null) return;
+            Model.Icon = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+
